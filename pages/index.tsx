3b63856140cceada14a71a257988571e6f484244
philManWithBeard <48831325+philManWithBeard@users.noreply.github.com>
2021-11-12 14:51:15 +0000

    more images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Hero from "../src/sections/hero/Hero";
+import ReverseHero from "../src/sections/hero/ReverseHero";
 import LandingLayout from "../src/layouts/LandingLayout";
 
 export default function Landing() {
@@ -8,8 +9,15 @@ export default function Landing() {
     <LandingLayout>
       <Hero
         title="Pick your Path to becoming a full stack developer"
-        subtitle="Our bootcamp is designed to fit around your experience and your aspirations. You only have to pay for the modules you need"
-        image="https://source.unsplash.com/collection/404339/800x600"
+        subtitle="Our bootcamp is designed to fit around your experience and your aspirations. You only have to pay for the modules you need."
+        image="https://github.com/philManWithBeard/courserio/blob/main/public/pablo-965.png?raw=true"
+        ctaText="Create your account now"
+        ctaLink="/signup"
+      />
+      <ReverseHero
+        title="Meet the Mentor who will guide you through your journey"
+        subtitle="Your mentor will help you when you get stuck and reach out when your confidence drops"
+        image="https://github.com/philManWithBeard/courserio/blob/main/public/pablo-965.png?raw=true"
         ctaText="Create your account now"
         ctaLink="/signup"
       />
