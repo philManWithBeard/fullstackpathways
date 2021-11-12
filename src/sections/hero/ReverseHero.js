81037f11106696585d0545878c36fa9a123974c5
philManWithBeard <48831325+philManWithBeard@users.noreply.github.com>
2021-11-12 14:51:55 +0000

    more images

--- src/sections/hero/ReverseHero.js
+++ src/sections/hero/ReverseHero.js
@@ -10,7 +10,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 
-export default function Hero({
+export default function ReverseHero({
   title,
   subtitle,
   image,
@@ -28,6 +28,10 @@ export default function Hero({
       px={8}
       mb={16}
       {...rest}>
+      <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
+        {/* TODO: Make this change every X secs */}
+        <Image src={image} size="100%" rounded="1rem" shadow="2xl" />
+      </Box>
       <Stack
         spacing={4}
         w={{ base: "80%", md: "40%" }}
@@ -60,15 +64,11 @@ export default function Hero({
           {ctaText}
         </Button>
       </Stack>
-      <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
-        {/* TODO: Make this change every X secs */}
-        <Image src={image} size="100%" rounded="1rem" shadow="2xl" />
-      </Box>
     </Flex>
   );
 }
 
-Hero.propTypes = {
+ReverseHero.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
   image: PropTypes.string,
@@ -76,7 +76,7 @@ Hero.propTypes = {
   ctaLink: PropTypes.string,
 };
 
-Hero.defaultProps = {
+ReverseHero.defaultProps = {
   title: "React landing page with Chakra UI",
   subtitle:
     "This is the subheader section where you describe the basic benefits of your product",
